659fac9ee271973e1f619da49d2ac7d1ae02999f
Anish Velagapudi <16AnishV@gmail.com>
2016-10-11 14:34:31 -0700

    Changed syntax

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,16 +1,17 @@
 # Exercise 2: Creating data frames
 
 # Create a vector of the number of points the Seahawks have scored in each game this season (google "Seahawks")
-Points <- c(10, 3, 37, 27)
+points <- c(10, 3, 37, 27)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored against them in each game this season
-PtsAllowed <- c(12, 9, 18, 17)
+points.allowed <- c(12, 9, 18, 17)
 
 # Combine your two vectors into a dataframe
-data <- data.frame(Points, PtsAllowed)
+data <- data.frame(points, points.allowed)
 
 # Create a new column "diff" that is the difference in points
-data[["diff"]] <- (abs(Points-PtsAllowed))
+data$diff <- data$points - data$points.allowed
+# data[["diff"]] <- (abs(points-points.allowed))
 
 # Create a new column "won" which is TRUE if the Seahawks wom
 data[["won"]] <- c(TRUE, FALSE, TRUE, TRUE)
